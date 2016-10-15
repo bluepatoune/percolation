1e5bc8b84107fd8a9f2f9329708cb08a30a1cfb3
bluepatoune <bluepatoune@gmail.com>
2016-10-15 17:09:51 +0200

    Add files via upload
    Version 2 avec indice de porosité.

--- RAndomisation du sol.py
+++ RAndomisation du sol.py
@@ -1,37 +1,44 @@
 import random as rd
 
-def matrice(n,p,i):
+EAU = 2
+VIDE = 1
+PIERRE = 0
+
+def matrice(n, p, i):
     """Création d'une matrice modélisant une roche poreuse aléatoire. 0 indique la roche , 1 indique un pore """
-    S=zero(n,p)
-    S=pores(S,i)
-    S=bords(S)
+    S = zero(n,p)
+    S = pores(S,i)
+    S = bords(S)
     matshow(S)
     return S
     
-def zero(n,p):
+def zero(n, p):
     """ crééer un ematrice de zéros de taille n,p """ 
-    S=[0]*n
-    for e in range(n):
-        S[e]=[0]*p
+    S = [0]*n
+    for i in range(n):
+        S[i] = [0]*p
     return S
 
-def pores(S,i):
-    """ introduit des pores au hasard  en fobnction de l'indice de porosité i """
+def pores(S, i):
+    """ introduit des pores au hasard  en fonction de l'indice de porosité i """
     for x in range(len(S)):
         for y in range(len(S[0])):
-            
-            S[x][y]=rd.choice([0,0,1,1,1])
+            S[x][y] = bernoulli(i)
     return S
     
-def liste (i):
-    """ retourne une liste de 0 et de 1'en fonction de l'indice de porosité compris entre """
-    
+def bernoulli (i):
+    """ retourne un 1 ou 0 en fonction de la propabilité p."""
+    x = rd.random()
+    if x < i:
+        return 1
+    else:
+        return 0
 
 def bords(S):
     """ on borde la matrice de -1 """
     for x in range(len(S)):
         S[x].insert(0,-1)
         S[x].append(-1)
-    L=[-1]*len(S[0])
+    L = [-1]*len(S[0])
     S.append(L)
     return S
