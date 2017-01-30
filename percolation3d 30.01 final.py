5c74deb7f57e791572ecd0af4a9645cf56566a7c
bluepatoune <bluepatoune@gmail.com>
2017-01-30 15:00:16 +0100

    Add files via upload

--- percolation3d 30.01 final.py
+++ percolation3d 30.01 final.py
@@ -19,6 +19,10 @@ couleurs = {NEANT:        None  ,
             VIDE:         None,
             EAU:         'blue' ,
             EAU_MOUVANTE:'cyan' }
+            
+# n = nombre de matrice (profondeur)
+# p = nombre de ligne dans chaque matrice 
+# q = nombre de coeff dans chaque ligne ( colonne ) 
 
 def draw(espace, subplot, clrs):
     """Dessine chaque coeficient de la matrice comme un point de plot 3D."""
@@ -28,29 +32,31 @@ def draw(espace, subplot, clrs):
                 if couleurs[coef] != None:
                     subplot.scatter(z, y, -x, c=couleurs[coef])
 
-def modelisation(n, p, q, dim, indice=0.5):
+def modelisation(n, p, q, dim, indice = 0.5, affichage = True):
     """réalisation d'une propagation d'eau à travers un sol rocheux, retourne True si il y a percolation """
     etape_1 = initialisation(n, p, q, indice)
     eau_mouvante1 = etape_1[1]
     esp = etape_1[0]
     liste_vecteurs = vecteurs_espace(dim) 
-    return percolation(esp, liste_vecteurs, eau_mouvante1)
+    return percolation(esp, liste_vecteurs, eau_mouvante1, affichage)
 
-def percolation(espace, liste_vecteurs, eau_mouvante1 ):
+def percolation(espace, liste_vecteurs, eau_mouvante1, affichage):
     """Affiche graphiquement la propagation de l'eau, et indique s'il y a percolation."""
-    fig = pyplot.figure()
-    subplot = fig.add_subplot(111, projection='3d')
-    subplot.set_xlabel('Z')
-    subplot.set_ylabel('Y')
-    subplot.set_zlabel('-X')
+    if affichage == True:
+        fig = pyplot.figure()
+        subplot = fig.add_subplot(111, projection='3d')
+        subplot.set_xlabel('Z')
+        subplot.set_ylabel('Y')
+        subplot.set_zlabel('-X')
 
-    draw([[[-1, 0, 1, 2, 3]]], subplot, couleurs)
-    pyplot.pause(1)
+        draw([[[-1, 0, 1, 2, 3]]], subplot, couleurs)
+        pyplot.pause(1)
 
     eau_mouvante = eau_mouvante1
     while eau_mouvante != []:
-        draw(espace, subplot, couleurs)
-        pyplot.pause(.0001)
+        if affichage == True:
+            draw(espace, subplot, couleurs)
+            pyplot.pause(.0001)
 
         eau_mouvante = propagation(espace, eau_mouvante, liste_vecteurs)
 
@@ -82,19 +88,38 @@ def resultat(espace):
                 return True
     return False
     
-"""def resultat(espace):
+def resultat(espace):
     matrice = espace[len(espace)-2]
     y = 0
     z = 0
-    while y <= len(matrice-2) and z <= len(matrice[0]-2) and (matrice[y][z] != EAU or matrice[y][z] != EAU_MOUVANTE):
-        i+=1
-    
-    if i>
-        return false
-    else:
-        return
-                return True
-    return False"""
+    p = len(matrice)-2
+    q = len(matrice[0])-2
+    while y <= p and (matrice[y][z] != EAU or matrice[y][z] != EAU_MOUVANTE):
+        z += 1
+        if z == q:
+            y += 1
+            z = 0
+    if y == p+1:
+        return False
+    else :
+        return True
+        
+def resultat(espace): # rendre ca jolie 
+    matrice = espace[len(espace)-2]
+    y = 0
+    z = 0
+    p = len(matrice)-2
+    q = len(matrice[0])-2
+    while y <= p and matrice[y][z] != EAU and matrice[y][z] != EAU_MOUVANTE:
+        if z != q:
+            z += 1
+        else: 
+            y += 1
+            z = 0
+    if y == p+1:
+        return False
+    else :
+        return True
 
     
 def vecteurs_espace(dim):
@@ -121,7 +146,6 @@ def infiltration(espace, pores_vides):
     for (x, y, z) in pores_vides:
         espace[x][y][z] = EAU_MOUVANTE
     return espace
-    
 
 def initialisation(n, p, q, indice):
     esp = espace(n, p, q, indice)
@@ -133,7 +157,6 @@ def initialisation(n, p, q, indice):
                 eau_mouvante.append((0, y, z))
     return esp, eau_mouvante
     
-
 def espace(n, p, q, indice=.5):
     """Création d'une matrice modélisant une roche poreuse aléatoire."""
     espace = zero(n, p, q)
@@ -173,4 +196,4 @@ def bords(espace): # TODO: Facile à généraliser en n dimensions avec une fonc
     return espace
 
 if __name__ == '__main__': # Fonction de test
-    print(modelisation(8, 8, 8, 3))
+    print(modelisation(8, 8, 8, 3, 0.9, False))
